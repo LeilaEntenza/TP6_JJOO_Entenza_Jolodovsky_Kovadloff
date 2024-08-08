0707577a63b701e94aea8f7867c3c95d39e6652b
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2024-08-08 13:48:45 -0300

    controller paises creado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,4 +23,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Paises(){
+      
+        return View("Paises");
+    }
 }
