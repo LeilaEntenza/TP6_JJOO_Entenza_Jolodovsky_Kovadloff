e9921c13285a731d5ea5dd54ffb688abccd54721
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2024-08-08 13:30:43 -0300

    proyecto creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP6_JJOO_Entenza_Jolodovsky_Kovadloff.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
